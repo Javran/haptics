59cfc4c9b0f8908716ec7a5654ee1f3a7b97713a
Javran Cheng <Javran.c@gmail.com>
2016-08-11 23:07:27 +0800

    conid

--- src/Trace/Haptics/Tix.hs
+++ src/Trace/Haptics/Tix.hs
@@ -2,15 +2,23 @@ module Trace.Haptics.Tix where
 
 import Data.Int
 import Data.Word
-import qualified Data.ByteString as BS
+import Data.Char
+import Data.Attoparsec.Text
+import qualified Data.Text as T
 
 data Tix = Tix [TixModule]
 
 type Hash = Word32
 
 data TixModule = TixModule
-  { tixModuleName :: BS.ByteString
+  { tixModuleName :: T.Text
   , tixModuleHash :: Hash
   , tixModuleListLen :: Int
   , tixModuleTixs :: [Int64]
   }
+
+parseConid :: Parser T.Text
+parseConid = do
+    c <- satisfy isUpper
+    cs <- takeWhile (\c -> isLower c || isUpper c || isDigit c || c == '\'')
+    pure (T.cons c cs)
